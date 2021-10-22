3f6c8041d02f08477afea7015071362f82eef00e
Abhinav Suman <abhinavsuman0204@gmail.com>
2021-10-22 14:07:53 +0530

    code changed

---
+++ python coding/1_string_and_list_objects.py
@@ -0,0 +1,32 @@
+#single word
+print("hello")
+
+# entire phrase
+
+print("this is a double quoted string")
+
+#we can use single quotes
+
+print('this is a  single quoted string')
+
+# multi line comment
+
+"""
+this is a multi line comment
+"""
+
+
+# print the length of the letters in the string
+
+print(len('Hello World'))
+
+
+#assign as a string
+
+s = 'Hello World'
+print(s)
+
+
+#string slicing
+
+print(s[::-1])
